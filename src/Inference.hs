aa118fca2785226795a9a43497125b8d3e2c2fa5
Garret Wassermann <gwasser@gmail.com>
2023-10-08 05:59:48 -0400

    better modeling of sequents and contexts

--- src/Inference.hs
+++ src/Inference.hs
@@ -1,6 +1,7 @@
 module Inference
     ( Rule (..),
       Sequent,
+      Context,
       Assumptions,
       ProofStep
     ) where
@@ -27,9 +28,12 @@ data Rule = Init -- only for use on Atoms
           | ImpliesImpliesLeft
           deriving (Eq, Show)
 
--- a sequent is a pair of a list of assumptions together with a proposition
-type Sequent = (Assumptions, Prop)
+-- a sequent is a pair of a context (assumptions) together with a proposition
+type Sequent = (Context, Prop)
+-- context is a pair of lists of invertible and non-invertible propositions
+type Context = (Assumptions,Assumptions)
 type Assumptions = [Prop]
 
 -- a step in the proof is the pair of a Sequent derived using a Rule
+-- meant to help create the actual proof and not just check for validity
 type ProofStep = (Rule, Sequent)
